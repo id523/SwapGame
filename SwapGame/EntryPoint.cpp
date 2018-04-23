68d37d740a15060f762228f6f0ed01d93d003f9e
id523 <edward.giles@dulwich-suzhou.cn>
2018-04-23 10:22:58 +0800

    Moved game-specific defines and methods to a new header file

--- SwapGame/EntryPoint.cpp
+++ SwapGame/EntryPoint.cpp
@@ -13,6 +13,7 @@
 #include "IMGi.h"
 #include "TTFi.h"
 
+#include "GameStates.h"
 #include "NineSlice.h"
 
 // "Conversion, possible loss of data"
@@ -20,20 +21,8 @@
 
 using namespace std;
 
-#define GAME_STATE int64_t
-#define STATE_BIT(n) (1LL << n)
-
-#define BOARD_WIDTH 6
-#define BOARD_HEIGHT 6
-#define BOARD_CELLS (BOARD_WIDTH * BOARD_HEIGHT)
-#define SQUARE_SIZE 80
-#define START_X 160
-#define START_Y 0
 #define ANIM_SPEED 0.35f
 #define HIGHLIGHT_MARGIN 10
-
-#define PLAYER_BLACK 1
-#define PLAYER_WHITE 2
 
 #define MAKE_RECT(VAR, X, Y, W, H) SDL_Rect VAR; VAR.x = X; VAR.y = Y; VAR.w = W; VAR.h = H
 #define MAKE_COLOR(VAR, R, G, B, A) SDL_Color VAR; VAR.r = R; VAR.g = G; VAR.b = B; VAR.a = A
@@ -48,82 +37,6 @@ DELETER_CLASS(TTF_Font, TTF_CloseFont);
 
 #define SAFEPTR(x) unique_ptr<x, x##_Deleter>
 #define MAKESAFE(t, v) unique_ptr<t, t##_Deleter> v##_P(v)
-
-GAME_STATE PerformSwap(GAME_STATE s, int sp1, bool vertical) {
-	int sp2 = sp1 + (vertical ? BOARD_WIDTH : 1);
-	bool bit1 = !!(s & STATE_BIT(sp1));
-	bool bit2 = !!(s & STATE_BIT(sp2));
-	s &= ~STATE_BIT(sp1);
-	s &= ~STATE_BIT(sp2);
-	if (bit1) s |= STATE_BIT(sp2);
-	if (bit2) s |= STATE_BIT(sp1);
-	return s;
-}
-
-void GetScreenPos(int pos, int& x, int& y) {
-	x = START_X + SQUARE_SIZE * (pos % BOARD_WIDTH);
-	y = START_Y + SQUARE_SIZE * (pos / BOARD_WIDTH);
-}
-
-bool GetMoveFromPos(int mx, int my, int& swapPos, bool& vertical) {
-	mx -= START_X;
-	my -= START_Y;
-	int gx = mx / SQUARE_SIZE;
-	int gy = my / SQUARE_SIZE;
-	if (gx < 0 || gy < 0 || gx >= BOARD_WIDTH || gy >= BOARD_HEIGHT) {
-		return false;
-	}
-	int rx = mx - gx * SQUARE_SIZE;
-	int ry = my - gy * SQUARE_SIZE;
-	if (rx >= ry) {
-		if (rx >= SQUARE_SIZE - ry) {
-			// Quadrant Right
-			if (gx < BOARD_WIDTH - 1) {
-				swapPos = gy * BOARD_WIDTH + gx;
-				vertical = false;
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			// Quadrant Up
-			if (gy > 0) {
-				swapPos = (gy - 1) * BOARD_WIDTH + gx;
-				vertical = true;
-				return true;
-			} else {
-				return false;
-			}
-		}
-	} else {
-		if (rx >= SQUARE_SIZE - ry) {
-			// Quadrant Down
-			if (gy < BOARD_HEIGHT - 1) {
-				swapPos = gy * BOARD_WIDTH + gx;
-				vertical = true;
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			// Quadrant Left
-			if (gx > 0) {
-				swapPos = gy * BOARD_WIDTH + gx - 1;
-				vertical = false;
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-}
-const GAME_STATE TopRowMask = (1LL << BOARD_WIDTH) - 1;
-const GAME_STATE BottomRowMask = TopRowMask << (BOARD_WIDTH * (BOARD_HEIGHT - 1));
-int GetWinner(GAME_STATE s) {
-	if ((s & TopRowMask) == 0) return PLAYER_BLACK;
-	if ((s & BottomRowMask) == BottomRowMask) return PLAYER_WHITE;
-	return 0;
-}
 
 float lerp(float a, float b, float x) {
 	return a * (1 - x) + b * x;
---
+++ SwapGame/GameStates.h
@@ -0,0 +1,88 @@
+#define GAME_STATE long long
+#define STATE_BIT(n) (1LL << n)
+
+#define BOARD_WIDTH 6
+#define BOARD_HEIGHT 6
+#define BOARD_CELLS (BOARD_WIDTH * BOARD_HEIGHT)
+#define SQUARE_SIZE 80
+#define START_X 160
+#define START_Y 0
+
+#define PLAYER_BLACK 1
+#define PLAYER_WHITE 2
+
+GAME_STATE PerformSwap(GAME_STATE s, int sp1, bool vertical) {
+	int sp2 = sp1 + (vertical ? BOARD_WIDTH : 1);
+	bool bit1 = !!(s & STATE_BIT(sp1));
+	bool bit2 = !!(s & STATE_BIT(sp2));
+	s &= ~STATE_BIT(sp1);
+	s &= ~STATE_BIT(sp2);
+	if (bit1) s |= STATE_BIT(sp2);
+	if (bit2) s |= STATE_BIT(sp1);
+	return s;
+}
+
+void GetScreenPos(int pos, int& x, int& y) {
+	x = START_X + SQUARE_SIZE * (pos % BOARD_WIDTH);
+	y = START_Y + SQUARE_SIZE * (pos / BOARD_WIDTH);
+}
+
+bool GetMoveFromPos(int mx, int my, int& swapPos, bool& vertical) {
+	mx -= START_X;
+	my -= START_Y;
+	int gx = mx / SQUARE_SIZE;
+	int gy = my / SQUARE_SIZE;
+	if (gx < 0 || gy < 0 || gx >= BOARD_WIDTH || gy >= BOARD_HEIGHT) {
+		return false;
+	}
+	int rx = mx - gx * SQUARE_SIZE;
+	int ry = my - gy * SQUARE_SIZE;
+	if (rx >= ry) {
+		if (rx >= SQUARE_SIZE - ry) {
+			// Quadrant Right
+			if (gx < BOARD_WIDTH - 1) {
+				swapPos = gy * BOARD_WIDTH + gx;
+				vertical = false;
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			// Quadrant Up
+			if (gy > 0) {
+				swapPos = (gy - 1) * BOARD_WIDTH + gx;
+				vertical = true;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	} else {
+		if (rx >= SQUARE_SIZE - ry) {
+			// Quadrant Down
+			if (gy < BOARD_HEIGHT - 1) {
+				swapPos = gy * BOARD_WIDTH + gx;
+				vertical = true;
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			// Quadrant Left
+			if (gx > 0) {
+				swapPos = gy * BOARD_WIDTH + gx - 1;
+				vertical = false;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+const GAME_STATE TopRowMask = (1LL << BOARD_WIDTH) - 1;
+const GAME_STATE BottomRowMask = TopRowMask << (BOARD_WIDTH * (BOARD_HEIGHT - 1));
+int GetWinner(GAME_STATE s) {
+	if ((s & TopRowMask) == 0) return PLAYER_BLACK;
+	if ((s & BottomRowMask) == BottomRowMask) return PLAYER_WHITE;
+	return 0;
+}
